96784e7aaa55327a97a07627df308d08bc893e6d
Falk Michel <falk.michel@motius.de>
2018-01-06 19:04:51 +0100

    added quota limitation

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ function iptablesArgs (rule) {
     if (rule.out) args = args.concat(["-o", rule.out]);
     if (rule.target) args = args.concat(["-j", rule.target]);
     if (rule.list) args = args.concat(["-n", "-v"]);
+    if (rule .quota) args = args.concat(["-m quota --quota", rule.quota]);
 
     return args;
 }
